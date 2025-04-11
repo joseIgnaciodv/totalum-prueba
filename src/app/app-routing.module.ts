97a0033a71f0704bb824dab74510e344490125c9
joseIgnaciodv <joseignaciodelvallebustillo@gmail.com>
2025-04-11 21:14:41 +0200

    add the 3 main routes ("/pedidos", "/clientes", "/productos") and a default ("") route that redirects to "/pedidos" route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductosComponent } from './components/productos/productos.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'productos', component: ProductosComponent },
+  { path: 'pedidos', component: PedidosComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: '', redirectTo: '/pedidos', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
