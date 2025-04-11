aacc79c3e257f1796472c032451d0b36dcad45ef
joseIgnaciodv <joseignaciodelvallebustillo@gmail.com>
2025-04-11 21:13:28 +0200

    add the 3 main components and the formsModule, to the main app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PedidosComponent,
+    ClientesComponent,
+    ProductosComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
